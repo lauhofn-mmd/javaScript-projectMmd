930cb7dba81fc62720075250245c126723e8c108
ThatOneGemini <sifegjensen@gmail.com>
2026-02-10 11:03:21 +0100

    Update sif_javaScript.js

---
+++ sif_javaScript.js
@@ -0,0 +1,24 @@
+const translation = {
+    dk: {
+        header: "Velkommen til OilsByNature",
+        topMenu_venster: "Søg",
+        topMenu_tekst: "Valuta",
+        hjem: "Hjem",
+        butik: "Butik",
+        omOS: "Om os",
+        kontakt: "Kontakt",
+        blog: "Blog",
+        limited_storTekst: "Vinterens fire essentials Fire olier - én komplet vinterplejerutine. Det perfekte julegavesæt",
+        button: "Shop nu",
+        h2: "Mød vores produkter",
+        product_title1: "Økologisk Koldpresset Mandelolie",
+        product_title2: "Økologisk Koldpresset Abrikoskerneolie",
+        product_title3: "Økologisk Koldpresset Arganolie",
+        product_title4: "Økologisk Koldpresset Jojobaolie",
+        button: "Læg i indkøbskurv",
+        review1: "Den her jojobaolie er simpelthen skøn - dejlig tynd, nem at smøre ud og trækker hurtigt ind i huden. Den er jeg især fan af",
+        review2: "Min kone har testet selve olien, da hun bruger arganolie til hverdag. Hun synes at olien var af god kvalitet og tykkere end den hun normalt bruger. Den var både god til hår og hud. Især den måde den tørrede ind på.",
+        review3: "Jeg er massør og jeg er helt vild med jeres mandelolie - det er blødt, har den helt perfekt konsistens (ikke for fedtet og tykt), det føles dejligt på huden og fungerer helt perfekt som massageolie. Nok den bedste, jeg har haft i hænderne og jeg har trods alt 12 års erfaring som massør :)",
+        review4: "Jeg elsker jeres abrikoskerneolie - trækker super hurtigt ind i huden, mærkes dejlig blødt og er skøn i konsistensen. Som hudplejeprodukt er den helt fantastisk."
+    }
+}
