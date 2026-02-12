00952436093f8230c16dd975e1135653f80ba88a
lauhofn-mmd <lauhofn@gmail.com>
2026-02-12 13:02:04 +0100

    lau burger virker js :)

--- lau2-js.js
+++ lau2-js.js
@@ -14,13 +14,3 @@ for (let i = 0; i < menuItems.length; i++) {
     '<a href="' + menuItems[i].href + '" target="' + menuItems[i].target + '">' + menuItems[i].name + '</a>' +
   '</div>';
 }
-
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-document.getElementById("mySidepanel").style.width = "500px";
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-document.getElementById("mySidepanel").style.width = "0";
-}
